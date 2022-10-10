c69d8fd6afa6a666eca8a0566b4ed75a3a759e20
mohammedramadan99 <lifeisagame0101@gmail.com>
2022-10-10 14:18:02 +0200

    ds

--- pages/api/posts/likes.jsx
+++ pages/api/posts/likes.jsx
@@ -6,69 +6,70 @@ const handler = nc();
 
 handler.use(isAuth).put(async (req, res) =>
 {
-    //1.Find the post to be liked
-    const { postId } = req?.body;
-    const post = await Post.findById(postId);
-    //2. Find the login user
-    const loginUserId = req?.user?.id;
-    console.log(req.user);
-    //3. Find is this user has liked this post?
-    const isLiked = post?.isLiked;
-    //4.Chech if this user has dislikes this post
-    const alreadyDisliked = post?.disLikes?.find(
-        (userId) => userId?.toString() === loginUserId?.toString()
-    );
-    //5.remove the user from dislikes array if exists
-    if (alreadyDisliked)
-    {
-        const post = await Post.findByIdAndUpdate(
-            postId,
-            {
-                $pull: { disLikes: loginUserId },
-                isDisLiked: false,
-                reactionClass: "like",
-            },
-            {
-                new: true,
-                runValidators: true,
-            }
-        );
-        res.json(post);
-    }
-    //Toggle
-    //Remove the user if he has liked the post
-    if (isLiked)
-    {
-        const post = await Post.findByIdAndUpdate(
-            postId,
-            {
-                $pull: { likes: loginUserId },
-                isLiked: false,
-                reactionClass: "none",
-            },
-            {
-                new: true,
-                runValidators: true,
-            }
-        );
-        res.json(post);
-    } else
-    {
-        //add to likes
-        const post = await Post.findByIdAndUpdate(
-            postId,
-            {
-                $push: { likes: loginUserId },
-                isLiked: true,
-                reactionClass: "like",
-            },
-            {
-                new: true,
-                runValidators: true,
-            }
-        );
-        res.json(post);
-    }
+    // //1.Find the post to be liked
+    // const { postId } = req?.body;
+    // const post = await Post.findById(postId);
+    // //2. Find the login user
+    // const loginUserId = req?.user?.id;
+    // console.log(req.user);
+    // //3. Find is this user has liked this post?
+    // const isLiked = post?.isLiked;
+    // //4.Chech if this user has dislikes this post
+    // const alreadyDisliked = post?.disLikes?.find(
+    //     (userId) => userId?.toString() === loginUserId?.toString()
+    // );
+    // //5.remove the user from dislikes array if exists
+    // if (alreadyDisliked)
+    // {
+    //     const post = await Post.findByIdAndUpdate(
+    //         postId,
+    //         {
+    //             $pull: { disLikes: loginUserId },
+    //             isDisLiked: false,
+    //             reactionClass: "like",
+    //         },
+    //         {
+    //             new: true,
+    //             runValidators: true,
+    //         }
+    //     );
+    //     res.json(post);
+    // }
+    // //Toggle
+    // //Remove the user if he has liked the post
+    // if (isLiked)
+    // {
+    //     const post = await Post.findByIdAndUpdate(
+    //         postId,
+    //         {
+    //             $pull: { likes: loginUserId },
+    //             isLiked: false,
+    //             reactionClass: "none",
+    //         },
+    //         {
+    //             new: true,
+    //             runValidators: true,
+    //         }
+    //     );
+    //     res.json(post);
+    // } else
+    // {
+    //     //add to likes
+    //     const post = await Post.findByIdAndUpdate(
+    //         postId,
+    //         {
+    //             $push: { likes: loginUserId },
+    //             isLiked: true,
+    //             reactionClass: "like",
+    //         },
+    //         {
+    //             new: true,
+    //             runValidators: true,
+    //         }
+    //     );
+    //     res.json(post);
+    // }
+    res.json("like")
 })
 export const config = {
     api: {
