54919376bc2ad9b10f9766a4ed1ef9d73d844054
mohammedramadan99 <lifeisagame0101@gmail.com>
2022-10-13 00:13:05 +0200

    sd

--- components/MainPage/Comment.jsx
+++ components/MainPage/Comment.jsx
@@ -1,11 +1,14 @@
+import Image from 'next/image'
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 export default function Comment({ comment })
 {
+    const {loading} = useSelector(state => state.comments)
     return (
         <div className="mainPage__middle__posts__container__commentsGroupe__comments__comment">
             <div className="mainPage__middle__posts__container__commentsGroupe__comments__comment__img img__rounded">
-                <img src={comment?.user?.profilePhoto} alt="img" />
+                {comment?.user?.profilePhoto && <Image src={comment?.user?.profilePhoto} alt="img" width={100} height={100} />} 
             </div>
             <div className="mainPage__middle__posts__container__commentsGroupe__comments__comment__details">
                 <div className="mainPage__middle__posts__container__commentsGroupe__comments__comment__details__name"> {comment?.user?.name} </div>
